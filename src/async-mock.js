a1df8fab16d96acb2021fb3ef09c4f8e3bd70565
Jesús Nuñez <79812212+JesuNunez@users.noreply.github.com>
2024-06-11 11:50:44 -0300

    Update async-mock.js

--- src/async-mock.js
+++ src/async-mock.js
@@ -1,10 +1,43 @@
 import data from './data.json';
 
-const fetchMockData = () => {
+const fetchMockData = (productId) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(data);
-    }, 1000); 
+      const item = data.find((product) => product.id === productId);
+      resolve(item);
+    }, 2000); 
+  });
+};
+
+export const addToCart = (cartItems, setCartItems, item) => {
+  setCartItems((prevItems) => {
+    const existingItem = prevItems.find((i) => i.id === item.id);
+    if (existingItem) {
+      return prevItems.map((i) =>
+        i.id === item.id ? { ...i, qty: i.qty + 1 } : i
+      );
+    }
+    return [...prevItems, { ...item, qty: 1 }];
+  });
+};
+
+export const removeFromCart = (cartItems, setCartItems, id) => {
+  setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+};
+
+export const handleFilterChange = (setFilter, newFilter) => {
+  setFilter(newFilter);
+};
+
+export const handleDescriptionClick = (setShowFilters) => {
+  setShowFilters(false);
+};
+
+export const updateCartQuantity = (cartItems, setCartItems, id, newQty) => {
+  setCartItems((prevItems) => {
+    return prevItems.map((item) =>
+      item.id === id ? { ...item, qty: newQty } : item
+    );
   });
 };
 
